64929cf9cc9b7b44535428392560129b2be01a93
Ronald Haentjens Dekker <ronald.dekker@huygens.knaw.nl>
2016-03-15 18:10:18 +0100

    TextGraph DOT exporter: added vertices for annotations nodes to DOT export of the text graph.

--- xml_collation/test_text_graph_dot_output.py
+++ xml_collation/test_text_graph_dot_output.py
@@ -5,7 +5,7 @@ from xml_collation.TextGraph import convert_superwitness_to_textgraph
 from xml_collation.collate_xml_hierarchy import convert_xml_string_into_tokens, align_tokens_and_return_superwitness
 
 
-def export_as_dot(textgraph):
+def export_as_dot(textgraph, annotations=False):
     # opener
     output = "digraph TextGraph {\n"
 
@@ -23,6 +23,13 @@ def export_as_dot(textgraph):
     # add edges for text nodes
     for v, w in pairwise(text_tokens_as_numbers):
         output += '    '+str(v)+' -> '+str(w)+'\n'
+
+    if annotations:
+        # I need to sort the annotations that are on the text graph
+        annotation_counter = 0
+        for annotation in textgraph.annotations_sorted:
+            annotation_counter += 1
+            output += '    a'+str(annotation_counter)+' label="'+annotation.tagname+'"\n'
 
     # closer
     output += "}"
@@ -52,5 +59,34 @@ class DotTest(TestCase):
     1 -> 2
     2 -> 3
 }"""
+        self.assertEqual(expected_out, dot_export)
+
+
+# for the next test there are again two things...
+# vertices for the markup
+# how do we make sure that we influence the previous test too much
+# note that annotation vertices
+# need to have unique id (in comparison to the text vertices)
+
+    def test_dot_markup_only(self):
+        witness_a = "<tei><s>x y z</s></tei>"
+        witness_b = "<tei><s>x</s>y<s>z</s></tei>"
+        tokens_a = convert_xml_string_into_tokens(witness_a)
+        tokens_b = convert_xml_string_into_tokens(witness_b)
+        superwitness = align_tokens_and_return_superwitness(tokens_a, tokens_b)
+        textgraph = convert_superwitness_to_textgraph(superwitness)
+        dot_export = export_as_dot(textgraph, annotations=True)
+        expected_out = """digraph TextGraph {
+    1 label="x"
+    2 label="y"
+    3 label="z"
+    1 -> 2
+    2 -> 3
+    a1 label="tei"
+    a2 label="s"
+    a3 label="s"
+    a4 label="s"
+}"""
+        # vertices only for the moment
         self.assertEqual(expected_out, dot_export)
 
